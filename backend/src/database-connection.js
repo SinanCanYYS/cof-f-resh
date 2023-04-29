6fcdcbbe7765b8c5c76ff394e91c4d5f0f04f2f5
SinanC <125397960+SinanCanYYS@users.noreply.github.com>
2023-04-29 06:59:15 +0000

    Change app title to coffresh

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -3,8 +3,3 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
   .then(() => console.log('Connected to MongoDB', process.env.MONGODB_CONNECTION_STRING))
   .catch((err) => console.error('Could not connect to MongoDB...', err))
-
-// const Cat = mongoose.model('Cat', { name: String })
-
-// const kitty = new Cat({ name: 'pamuk' })
-// kitty.save().then(() => console.log('meow'))
