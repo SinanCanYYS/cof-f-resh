d81dd23d3530878cebff1cfd85ba2ab89d0c9ed1
SinanC <125397960+SinanCanYYS@users.noreply.github.com>
2023-05-01 21:04:30 +0000

    Add MenuView, IngredientView, NewMenuItemView

---
+++ frontend/src/views/MenuView.vue
@@ -0,0 +1,48 @@
+<script>
+// import axios from 'axios'
+// import { useAccountStore } from '../stores/account'
+import { mapActions, mapState } from 'pinia'
+import { useRestaurantStore } from '../stores/restaurant'
+
+// axios.defaults.withCredentials = true
+// axios.defaults.baseURL = import.meta.env.VITE_API_URL
+
+export default {
+  name: 'MenuView',
+  data() {
+    return {
+      id: null,
+      restaurant: {},
+      menu: []
+    }
+  },
+  // created() {
+  //   this.id = this.$route.params.id
+  // },
+  async created() {
+    await this.calledRestaurant()
+  },
+  computed: {
+    // ...mapState(useAccountStore, ['user']),
+    // ...mapState(useRestaurantStore, ['restaurant'])
+  },
+  methods: {
+    ...mapActions(useRestaurantStore, ['fetchRestaurant']),
+    async calledRestaurant() {
+      this.restaurant = await this.fetchRestaurant(this.$route.params.id)
+      // this.menu.push(...restaurantMenu)
+    }
+  }
+}
+</script>
+<template lang="pug">
+h1 Menu of {{ restaurant.name }}
+br
+div
+  div(v-for="item in restaurant.menu" :key="item._id")
+    h3 {{ item.name }} - {{ item.type  }} - {{ item.subType }} - {{ item.price }}
+
+  button(@click="$router.push(`/restaurants/${this.$route.params.id}/menu/newmenuitem`)") Add Menu Item
+  button(@click="$router.push(`/restaurants/${this.$route.params.id}/menu/${item._id}`)") Edit
+  button(@click="$router.push(`/restaurants/${this.$route.params.id}/menu/${item._id}`)") Delete
+</template>
---
+++ frontend/src/views/NewMenuItemView.vue
@@ -0,0 +1,55 @@
+<script>
+// import axios from 'axios'
+import { useRestaurantStore } from '../stores/restaurant'
+import { useAccountStore } from '../stores/account'
+import { mapState, mapActions } from 'pinia'
+
+export default {
+  name: 'NewMenuItemView',
+  data() {
+    return {
+      name: '',
+      type: '',
+      subType: '',
+      price: ''
+    }
+  },
+  computed: {
+    ...mapState(useAccountStore, ['user'])
+  },
+  methods: {
+    ...mapActions(useRestaurantStore, ['addMenuItem']),
+    async doAddMenuItem() {
+      // const restaurant = useRestaurantStore().restaurant._id
+      await this.addMenuItem(
+        this.user._id,
+        this.$route.params.id,
+        this.name,
+        this.type,
+        this.subType,
+        this.price
+      )
+      this.$router.push(`/restaurants/${this.$route.params.id}/menu`)
+    }
+  }
+}
+</script>
+<template lang="pug">
+h1 Add New Menu Item
+
+form(@submit.prevent="doAddMenuItem")
+  div
+    label(for="name") Name
+    input#name(v-model="name")
+  div
+    label(for="type") Type
+    input#type(v-model="type")
+  div
+    label(for="subType") Sub Type
+    input#subType(v-model="subType")
+  div
+    label(for="price") Price
+    input#price(v-model="price")
+  div
+    button(type="submit") Add Menu Item
+</template>
