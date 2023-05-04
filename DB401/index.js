fe44b367bf6df9450566a9e04cb0d26b6c36cf31
SinanC <125397960+SinanCanYYS@users.noreply.github.com>
2023-05-04 08:48:02 +0000

    Add MongoDB aggregation

--- DB401/index.js
+++ DB401/index.js
@@ -5,50 +5,25 @@ const Movie = require('./models/movie')
 const Theater = require('./models/theater')
 
 async function main() {
-  const theatersByState = await Theater.aggregate([
-    { $group: { _id: '$location.address.state', theaters: { $push: '$theaterId' }, numberOfTheaters: { $sum: 1 } } },
-    { $set: { state: '$_id' } },
-    { $unset: '_id' },
-    { $sort: { numberOfTheaters: -1 } },
+  const popularMoviesByGenre = await Movie.aggregate([
+    { $unwind: '$genres' },
+    { $match: { 'imdb.rating': { $gt: 0 } } },
+    { $sort: { 'imdb.rating': -1 } },
+
     {
       $group: {
-        _id: 1,
-        mostCrowdedState: { $first: '$state' },
-        mostCrowdedCount: { $first: '$numberOfTheaters' },
-        leastCrowdedState: { $last: '$state' },
-        leastCrowdedCount: { $last: '$numberOfTheaters' },
+        _id: '$genres',
+        movies: { $push: { title: '$title', rating: '$imdb.rating' } },
       },
     },
     {
       $project: {
         _id: 0,
-        mostCrowdedState: {
-          name: '$mostCrowdedState',
-          numberOfTheaters: '$mostCrowdedCount',
-        },
-        leastCrowdedState: {
-          name: '$leastCrowdedState',
-          numberOfTheaters: '$leastCrowdedCount',
-        },
+        genre: '$_id',
+        movies: { $slice: ['$movies', 3] },
       },
     },
   ])
-
-  console.log(theatersByState)
-
-  const popularmoviesByDayOfWeek = await Movie.aggregate([
-    {
-      $group: {
-        _id: { $isoDayOfWeek: '$released' },
-        count: { $sum: 1 },
-        avgRating: { $avg: '$imdb.rating' },
-        stdDev: { $stdDevPop: '$imdb.rating' },
-      },
-    },
-    { $sort: { avgRating: -1 } },
-  ])
-
-  console.log(popularmoviesByDayOfWeek)
+  console.log(JSON.stringify(popularMoviesByGenre, null, 2))
 }
-
 main()
